eb811c1174b961d3e142aca22978b2e44e3f5b38
swaritnarang <narangswarit02@gmail.com>
2025-09-06 15:05:44 -0500

    basic file structrue

---
+++ src/util/configEditor.py
@@ -0,0 +1,8 @@
+def create_config():
+    pass
+
+def edit_value(value):
+    pass
+
+def get_config_value(key):
+    pass
---
+++ src/util/ollamaWrapper.py
@@ -0,0 +1,14 @@
+def start_server():
+    pass
+
+def list_models():
+    pass
+
+def pull_model(model):
+    pass
+
+def delete_model(model):
+    pass
+
+def run_prompt(model, prompt):
+    pass
